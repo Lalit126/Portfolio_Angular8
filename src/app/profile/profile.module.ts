d438e0f0d0db56a8b429166120e705942cc46afe
lalit126 <55103714+Lalit126@users.noreply.github.com>
2020-08-07 15:33:30 -0400

    changes module.ts

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
+// import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
 import { ProfileComponent } from './profile.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -20,7 +20,7 @@ import { ProjectsComponent } from './projects/projects.component';
   imports: [
     CommonModule,
     FormsModule,
-    SnotifyModule,
+    // SnotifyModule,
     NgxSpinnerModule
   ],
   declarations: [
@@ -38,8 +38,8 @@ import { ProjectsComponent } from './projects/projects.component';
     ProjectsComponent
   ],
   providers: [
-    { provide: 'SnotifyToastConfig', useValue: ToastDefaults},
-    SnotifyService,
+    // { provide: 'SnotifyToastConfig', useValue: ToastDefaults},
+    // SnotifyService,
   ]
 })
 export class ProfileModule { }
